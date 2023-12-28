d906652f4ba26332d383a68d04d6ea36722a6a33
Jasmine <jaasmine2001@gmail.com>
2023-12-28 08:45:13 +0530

    added basic setup

--- models/listing.js
+++ models/listing.js
@@ -8,14 +8,8 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        filename: {
-            type: String,
-            default: "listingimage",
-        },
-        url: {
             type: String,
             default: "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
-        },
     },
     price: Number,
     location: String,
