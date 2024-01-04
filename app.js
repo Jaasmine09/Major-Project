fc0e7123c1701aba872d73688ce010a4014d11d6
Jasmine <jaasmine2001@gmail.com>
2024-01-04 23:09:08 +0530

    added validation for reviews

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing");
+const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 main()
   .then(() => {
@@ -42,6 +43,17 @@ const validateListing = (req, res, next) => {
     next();
   }
 };
+
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 
 //Index Route
 app.get("/listings",wrapAsync(async (req, res) => {
@@ -105,6 +117,20 @@ app.delete(
     res.redirect("/listings");
   })
 );
+
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) =>{
+   let listing = await Listing.findById(req.params.id);
+   let newReview = new Review(req.body.review);
+
+   listing.reviews.push(newReview);
+
+   await newReview.save();
+   await listing.save();
+
+  res.redirect(`/listings/${listing._id}`);
+}));
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
--- models/listing.js
+++ models/listing.js
@@ -18,8 +18,8 @@ const listingSchema = new Schema({
         {
             type: Schema.Types.ObjectId,
             ref: "Review",
-        }
-    ]
+        },
+    ],
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
