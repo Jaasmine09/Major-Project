960d1b6df1850d80c86793d2ab82bb62fc34acad
Jasmine <jaasmine2001@gmail.com>
2024-01-04 21:09:28 +0530

    added reviews

--- app.js
+++ app.js
@@ -44,9 +44,7 @@ const validateListing = (req, res, next) => {
 };
 
 //Index Route
-app.get(
-  "/listings", validateListing, 
-  wrapAsync(async (req, res) => {
+app.get("/listings",wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
   })
